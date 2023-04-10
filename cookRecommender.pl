c788b419abce0d95257469232dab7df92d78b35f
yuefeng <maxonzhao@gmail.com>
2023-04-10 14:56:56 -0700

    project completed

--- cookRecommender.pl
+++ cookRecommender.pl
@@ -4,36 +4,38 @@ set_display :-
     getenv('DISPLAY', _), % Check if DISPLAY is already set
     !. % If so, do nothing
 set_display :-
-    process_create('sh', ['-c', 'export DISPLAY=154.5.139.248：8888'], []),
+    process_create('sh', ['-c', 'export DISPLAY=154.5.139.248�888'], []),
     setenv('DISPLAY', '154.5.139.248:0').
 
 collect(Query) :-
-            new(D, dialog('Cook Recommendation System')),
-            send(D, append(new(Label, label))),
-            send(Label, value('Hello, trying to figure out what to cook?
-            \nProvide your ingredients with a sentence (Conjunctive)
-            \nSample: What can I cook with cheese and pasta?')),
+    new(D, dialog('Cook Recommendation System')),
+    send(D, append(new(Label, label))),
+    send(Label, value('Hello, trying to figure out what to cook?
+    \nProvide your ingredients with a sentence (Conjunctive)
+    \nSample: What can I cook with cheese and pasta?')),
 
-            send(D, append(new(NameItem, text_item(query)))),
-            send(D, append(button(ok, message(D, return,
-                                            NameItem?selection)))),
-            send(D, append(button(cancel, message(D, return, @nil)))),
-            send(D, default_button(ok)),
-            get(D, confirm, Rval),
-            free(D), 
-            Rval \== @nil,
-            split_string(Rval, " -" ,"?.!-", Query).
+    send(D, append(new(NameItem, text_item(query)))),
+    send(D, append(button(ok, message(D, return,
+                                    NameItem?selection)))),
+    send(D, append(button(cancel, message(D, return, @nil)))),
+    send(D, default_button(ok)),
+    get(D, confirm, Rval),
+    free(D),
+    Rval \== @nil,
+    split_string(Rval, " -" ,"?.!-", Query).
 
 % NLP SECTION%
 % Parse a valid formatted sentence.
 parse_sentence(L0, L5, Ind) :-
-    subject(L0, L1, Ind), 
+    subject(L0, L1, Ind),
     verb(L1, L2, Ind),
     prep(L2, L3, Ind),
     end(L3, L4, Ind),
     nouns(L4, L5, Ind).
 
+% Definitions for various sentence components
 subject(["I" | L], L, _).
+
 verb(["cook" | L], L, _).
 verb(["make" | L], L, _).
 
@@ -55,51 +57,51 @@ end(L, L, _) :- !.
 noun([IngName | L], L, Ind) :- dish(Ind, Ing), name(Ing, IngName).
 
 % question(Question, QR, Ind) is true if Ind is  an answer to Question
-question(["What", "can" | L0], L1, Ind) :- 
+question(["What", "can" | L0], L1, Ind) :-
     parse_sentence(L0, L1, Ind).
 
 % ask(Q, A) gives answer A to question Q
-ask(Q, A) :- 
+ask(Q, A) :-
     question(Q, [], A).
 
 
 % DATABASE OF FACTS TO BE QUERIED %
-% Pancakes 
+% Pancakes
 dish(pancakes, flour).
 dish(pancakes, milk).
 dish(pancakes, 0).
 
-% Oatmeal Cookies 
+% Oatmeal Cookies
 dish(oatmeal_cookies, oats).
 dish(oatmeal_cookies, flour).
 dish(oatmeal_cookies, 1).
 
-% Cornbread 
+% Cornbread
 dish(cornbread, flour).
 dish(cornbread, cornmeal).
 dish(cornbread, 2).
 
-% Spaghetti Carbonara 
+% Spaghetti Carbonara
 dish(spaghetti_carbonara, pasta).
 dish(spaghetti_carbonara, cheese).
 dish(spaghetti_carbonara, 3).
 
-% Apple Crisp 
+% Apple Crisp
 dish(apple_crisp, apples).
 dish(apple_crisp, flour).
 dish(apple_crisp, 4).
 
-% Orange chicken 
+% Orange chicken
 dish(orange_chicken, oranges).
 dish(orange_chicken, chicken).
 dish(orange_chicken, 5).
 
-% Banana Bread 
+% Banana Bread
 dish(banana_bread, banana).
 dish(banana_bread, flour).
 dish(banana_bread, 6).
 
-% Smoothie 
+% Smoothie
 dish(smoothie, banana).
 dish(smoothie, apples).
 dish(smoothie, oranges).
@@ -109,41 +111,41 @@ dish(smoothie, milk).
 dish(smoothie, yogurt).
 dish(smoothie, 7).
 
-% avocado Toast 
+% avocado Toast
 dish(avocado_toast, avocado).
 dish(avocado_toast, cheese).
 dish(avocado_toast, 8).
 
-% Caesar Salad 
+% Caesar Salad
 dish(caesar_salad, lettuce).
 dish(caesar_salad, cheese).
 dish(caesar_salad, 9).
 
-% Stuffed chicken 
+% Stuffed chicken
 dish(stuffed_chicken, chicken).
 dish(stuffed_chicken, cheese).
 dish(stuffed_chicken, spinach).
 dish(stuffed_chicken, 10).
 
-% Veggie Soup 
+% Veggie Soup
 dish(veggie_soup, broccoli).
 dish(veggie_soup, carrot).
 dish(veggie_soup, tomatoes).
 dish(veggie_soup, beans).
 dish(veggie_soup, 11).
 
-% Grilled chicken 
+% Grilled chicken
 dish(grilled_chicken, chicken).
 dish(grilled_chicken, 12).
 
-% Mixed Stir Fry 
+% Mixed Stir Fry
 dish(mixed_stir_fry, beef).
 dish(mixed_stir_fry, broccoli).
 dish(mixed_stir_fry, carrot).
 dish(mixed_stir_fry, chicken).
 dish(mixed_stir_fry, 13).
 
-% Chili 
+% Chili
 dish(chili, beans).
 dish(chili, beef).
 dish(chili, yogurt).
@@ -155,18 +157,18 @@ dish(baked_salmon, salmon).
 dish(baked_salmon, rice).
 dish(baked_salmon, 15).
 
-% tofu Curry 
+% tofu Curry
 dish(tofu_curry, tofu).
 dish(tofu_curry, rice).
 dish(tofu_curry, carrot).
 dish(tofu_curry, 16).
 
-% Pizza 
+% Pizza
 dish(pizza, flour).
 dish(pizza, cheese).
 dish(pizza, 17).
 
-% Parfait 
+% Parfait
 dish(parfait, berries).
 dish(parfait, oats).
 dish(parfait, banana).
@@ -179,10 +181,10 @@ dish(cornmeal_fish_tacos, cornmeal).
 dish(cornmeal_fish_tacos, salmon).
 dish(cornmeal_fish_tacos, lettuce).
 dish(cornmeal_fish_tacos, beans).
-dish(cornmeal_fish_tacos, cheese). 
+dish(cornmeal_fish_tacos, cheese).
 dish(cornmeal_fish_tacos, 19).
 
-% pasta Primavera 
+% pasta Primavera
 dish(primavera, pasta).
 dish(primavera, lettuce).
 dish(primavera, spinach).
@@ -191,7 +193,7 @@ dish(primavera, carrot).
 dish(primavera, cheese).
 dish(primavera, 20).
 
-% Avocado and Tomato Sandwich 
+% Avocado and Tomato Sandwich
 dish(avocado_tomato_sandwich, flour).
 dish(avocado_tomato_sandwich, avocado).
 dish(avocado_tomato_sandwich, tomatoes).
@@ -199,7 +201,7 @@ dish(avocado_tomato_sandwich, lettuce).
 dish(avocado_tomato_sandwich, cheese).
 dish(avocado_tomato_sandwich, 21).
 
-% Veggie Fried rice 
+% Veggie Fried rice
 dish(veggie_fried_rice, rice).
 dish(veggie_fried_rice, tofu).
 dish(veggie_fried_rice, spinach).
@@ -208,7 +210,7 @@ dish(veggie_fried_rice, carrot).
 dish(veggie_fried_rice, tomatoes).
 dish(veggie_fried_rice, 22).
 
-% Salmon and avocado Sushi Rolls 
+% Salmon and avocado Sushi Rolls
 dish(salmon_avocado_roll, rice).
 dish(salmon_avocado_roll, salmon).
 dish(salmon_avocado_roll, avocado).
@@ -253,35 +255,32 @@ dish(fettucine_alfredo, milk).
 dish(fettucine_alfredo, pasta).
 dish(fettucine_alfredo, 30).
 
+% Name facts for every dish
 name(flour, "flour").
 name(rice, "rice").
 name(oats, "oats").
 name(cornmeal, "cornmeal").
 name(pasta, "pasta").
-
 name(apples, "apples").
 name(oranges, "oranges").
 name(bananas, "bananas").
 name(berries, "berries").
 name(avocado, "avocado").
-
 name(lettuce, "lettuce").
 name(spinach, "spinach").
 name(broccoli, "broccoli").
 name(carrot, "carrot").
 name(tomatoes, "tomatoes").
-
 name(chicken, "chicken").
 name(beef, "beef").
 name(beans, "beans").
 name(salmon, "salmon").
 name(tofu, "tofu").
-
 name(milk, "milk").
 name(cheese, "cheese").
 name(yogurt, "yogurt").
 
-% PROGRAM LOGIC FLOW %
+% PROGRAM LOGIC FLOW BASED ON USER MODE INPUT %
 parseOption("1", Recommendation) :-
     pickIngredients(Recommendation).
 parseOption("2", Ans) :-
@@ -299,84 +298,120 @@ start(Ans) :-
   write('\n2. Provide your ingredients with a sentence (Conjunctive)'),
   nl,
   read_line_to_string(user_input, St),
-  parseOption(St, Ans).
+  parseOption(St, Ans). 
 
 % Logical workflow for the random mode
 pickIngredients(Recommendation) :-
-    grainsQ(GrainsA),
-    fruitsQ(FruitsA),
-    veggiesQ(VeggiesA),
-    proteinsQ(ProteinsA),
-    dairyQ(DairyA),
-    max_list([GrainsA, FruitsA, VeggiesA, ProteinsA, DairyA], Upper),
-    min_list([GrainsA, FruitsA, VeggiesA, ProteinsA, DairyA], Lower),
-    random_between(Lower, Upper, Divisor),
-    DishNumber is mod(31,Divisor),
-    writeln("Based on your choices, I think you should make..."),
-    dish(Recommendation, DishNumber).
-    
+  % Ask the user to choose one food item from each category
+  grainsQ(GrainsA),
+  fruitsQ(FruitsA),
+  veggiesQ(VeggiesA),
+  proteinsQ(ProteinsA),
+  dairyQ(DairyA),
+  atom_number(GrainsA, X1),
+  atom_number(FruitsA, X2),
+  atom_number(VeggiesA, X3),
+  atom_number(ProteinsA, X4),
+  atom_number(DairyA, X5),
+  max_list([X1,X2,X3,X4,X5], Upper), % get the max value user entered
+  min_list([X1,X2,X3,X4,X5], Lower), % get the min value user entered
+  random_between(Lower, Upper, Divisor), % get random number between the values
+  DishNumber is mod(31,Divisor), % pick a random dish based on the above number out of the 30 dishes
+  writeln("Based on your choices, I think you should make..."),
+  dish(Recommendation, DishNumber).
+
+% Asks the user to pick a grain
 grainsQ(GrainsA) :-
-    write('Pick a grain!'),
-    write('\n1. flour'),
-    write('\n2. rice'),
-    write('\n3. oats'),
-    write('\n4. cornmeal'),
-    write('\n5. pasta'),
-    write('\nEnter your grain between 1-5: '),
-    read(GrainsA).
+  new(D, dialog('Cook Recommendation System')),
+  send(D, append(new(Label, label))),
+  send(Label, value('Hello, trying to figure out what to cook?
+  \nPick a grain: 1. flour 2. rice 3. oats 4. cornmeal 5. pasta
+  \nEnter your grain between 1-5: ')),
+  send(D, append(new(NameItem, text_item(query)))),
+  send(D, append(button(ok, message(D, return, NameItem?selection)))),
+  send(D, append(button(cancel, message(D, return, @nil)))),
+  send(D, default_button(ok)),
+  get(D, confirm, GrainsA),
+  free(D),
+  atom_number(GrainsA, X),
+  X > 1-1,
+  X < 5+1,			
+  GrainsA \== @nil.
 
+% Asks the user to pick a fruit
 fruitsQ(FruitsA) :-
-    write('Now, pick a fruit!'),
-    write('\n6. apples'),
-    write('\n7. oranges'),
-    write('\n8. banana'),
-    write('\n9. berries'),
-    write('\n10. avocado'),
-    write('\nEnter your fruit between 6-10: '),
-    read(FruitsA).
+  new(D, dialog('Cook Recommendation System')),
+  send(D, append(new(Label, label))),
+  send(Label, value('Now, pick a fruit!\n6. apples\n7. oranges\n8. banana\n9. berries\n10. avocado\nEnter your fruit between 6-10: ')),
+  send(D, append(new(NameItem, text_item(query)))),
+  send(D, append(button(ok, message(D, return, NameItem?selection)))),
+  send(D, append(button(cancel, message(D, return, @nil)))),
+  send(D, default_button(ok)),
+  get(D, confirm, FruitsA),
+  free(D),
+  atom_number(FruitsA, X),
+  X > 6-1,
+  X < 10+1,			
+  FruitsA \== @nil.
 
+% Asks the user to pick a veggie
 veggiesQ(VeggiesA) :-
-    write('Now, pick a veggie!'),
-    write('\n11. lettuce'),
-    write('\n12. spinach'),
-    write('\n13. broccoli'),
-    write('\n14. carrot'),
-    write('\n15. tomatoes'),
-    write('\nEnter your veggie between 11-15: '),
-    read(VeggiesA).
+  new(D, dialog('Cook Recommendation System')),
+  send(D, append(new(Label, label))),
+  send(Label, value('Now, pick a veggie!\n11. lettuce\n12. spinach\n13. broccoli\n14. carrot\n15. tomatoes\nEnter your veggie between 11-15: ')),
+  send(D, append(new(NameItem, text_item(query)))),
+  send(D, append(button(ok, message(D, return, NameItem?selection)))),
+  send(D, append(button(cancel, message(D, return, @nil)))),
+  send(D, default_button(ok)),
+  get(D, confirm, VeggiesA),
+  free(D),
+  atom_number(VeggiesA, X),
+  X > 11-1,
+  X < 15+1,			
+  VeggiesA \== @nil.
 
+% Asks the user to pick a protein
 proteinsQ(ProteinsA) :-
-    write('Now, pick a protein!'),
-    write('\n16. chicken'),
-    write('\n17. beef'),
-    write('\n18. beans'),
-    write('\n19. salmon'),
-    write('\n20. tofu'),
-    write('\nEnter your grain between 16-20: '),
-    read(ProteinsA).
+  new(D, dialog('Cook Recommendation System')),
+  send(D, append(new(Label, label))),
+  send(Label, value('Now, pick a protein!\n16. chicken\n17. beef\n18. beans\n19. salmon\n20. tofu\nEnter your grain between 16-20: ')),
+  send(D, append(new(NameItem, text_item(query)))),
+  send(D, append(button(ok, message(D, return, NameItem?selection)))),
+  send(D, append(button(cancel, message(D, return, @nil)))),
+  send(D, default_button(ok)),
+  get(D, confirm, ProteinsA),
+  free(D),
+  atom_number(ProteinsA, X),
+  X > 16-1,
+  X < 20+1,			
+  ProteinsA \== @nil.
 
+% Asks the user to pick a dairy product
 dairyQ(DairyA) :-
-    write('Finally, pick a dairy product!'),
-    write('\n21. milk'),
-    write('\n22. cheese'),
-    write('\n23. yogurt'),
-    write('\nEnter your dairy product between 21-23: '),
-    write('\n:'),
-    read(DairyA).
+  new(D, dialog('Cook Recommendation System')),
+  send(D, append(new(Label, label))),
+  send(Label, value('Finally, pick a dairy product!\n21. milk\n22. cheese\n23. yogurt\nEnter your dairy product between 21-23: ')),
+  send(D, append(new(NameItem, text_item(query)))),
+  send(D, append(button(ok, message(D, return, NameItem?selection)))),
+  send(D, append(button(cancel, message(D, return, @nil)))),
+  send(D, default_button(ok)),
+  get(D, confirm, DairyA),
+  free(D),
+  atom_number(DairyA, X),
+  X > 21-1,
+  X < 23+1,			
+  DairyA \== @nil.
 
 % Logical workflow for the conjunctive mode
 q(Ans) :-
-    write("Ask me: "), 
-    flush_output(current_output), 
-    read_line_to_string(user_input, St), 
+    write("Ask me: "),
+    flush_output(current_output),
+    read_line_to_string(user_input, St),
     split_string(St, " -", " ,?.!-", Ln), % ignore punctuation
     ask(Ln, Ans).
 q(Ans) :-
     write("No more answers\n"),
     q(Ans).
-
-% sample queries:
-% ?- ask(["What", "can", "I", "cook", "with", "flour", "and", "berries"], A).
 
 % Citations, some used in previous ideas that were scrapped
 % geography.pl from lecture
@@ -388,3 +423,5 @@ q(Ans) :-
 % https://www.swi-prolog.org/pldoc/man?predicate=random_between/3
 % https://stackoverflow.com/questions/39435709/how-to-remove-duplicates-from-a-list-in-swi-prolog
 % https://stackoverflow.com/questions/60571367/prolog-how-to-verify-user-input
+% https://www.swi-prolog.org/packages/xpce/examples.html
+% https://www.cs.ubc.ca/~poole/cs312/2023/prolog/geography_string.pl
--- gui.pl
+++
@@ -1,5 +0,0 @@
-:- dynamic(file_search_path/2).
-file_search_path(tcl, '/Users/yuefeng/Downloads/tcl8.6.13').
-file_search_path(tk, '/Users/yuefeng/Downloads/tk8.6.13').
-
-:- use_module(library(tcltk)).
